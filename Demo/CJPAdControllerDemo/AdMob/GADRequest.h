c33e6b263f2c2f4935a575e58207217730aec09c
Chris Phillips <chrisjp88@gmail.com>
2013-10-05 18:19:24 +0100

    Update AdMob to 6.5.1

--- Demo/CJPAdControllerDemo/AdMob/GADRequest.h
+++ Demo/CJPAdControllerDemo/AdMob/GADRequest.h
@@ -79,6 +79,30 @@ typedef enum {
 // @"Champs-Elysees Paris" or @"94041 US".
 - (void)setLocationWithDescription:(NSString *)locationDescription;
 
+// [Optional] This method allows you to specify whether you would like your app
+// to be treated as child-directed for purposes of the Children’s Online Privacy
+// Protection Act (COPPA) -
+// http://business.ftc.gov/privacy-and-security/childrens-privacy.
+//
+// If you call this method with YES, you are indicating that your app should be
+// treated as child-directed for purposes of the Children’s Online Privacy
+// Protection Act (COPPA).
+// If you call this method with NO, you are indicating that your app should not
+// be treated as child-directed for purposes of the Children’s Online Privacy
+// Protection Act (COPPA).
+// If you do not call this method, ad requests will include no indication of how
+// you would like your app treated with respect to COPPA.
+//
+// By setting this method, you certify that this notification is accurate and
+// you are authorized to act on behalf of the owner of the app.  You understand
+// that abuse of this setting may result in termination of your Google account.
+//
+// Note: it may take some time for this designation to be fully implemented in
+// applicable Google services.
+// This designation will only apply to ad requests for which you have set this
+// method.
+- (void)tagForChildDirectedTreatment:(BOOL)childDirectedTreatment;
+
 #pragma mark Contextual Information
 
 // A keyword is a word or phrase describing the current activity of the user
